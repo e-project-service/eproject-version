21d7f886fd0a260464b6900eddd6f9baa72ea00e
xiaoyao9184 <xiaoyao9184@gmail.com>
2021-03-18 17:05:37 +0800

    change JwtAuthenticationToken name

--- gateway/src/main/java/com/github/xiaoyao9184/eproject/version/SecurityConfig.java
+++ gateway/src/main/java/com/github/xiaoyao9184/eproject/version/SecurityConfig.java
@@ -6,19 +6,29 @@ import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.web.PathMappedEndpoints;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 /**
@@ -30,6 +40,9 @@ public class SecurityConfig {
 
     @Autowired
     PathMappedEndpoints pathMappedEndpoints;
+
+    @Autowired
+    OAuth2ClientProperties oAuth2ClientProperties;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(
@@ -43,7 +56,11 @@ public class SecurityConfig {
                 .oauth2Login()
                     .and()
                 .oauth2ResourceServer()
-                    .jwt();
+                    .jwt()
+                        .jwtAuthenticationConverter(
+                                new ReactiveJwtAuthenticationConverterAdapter(
+                                        new OAuth2ClientProviderUserNameAttributeJwtAuthenticationConverter(oAuth2ClientProperties)
+                                ));
 
         http
                 .authorizeExchange()
@@ -90,6 +107,45 @@ public class SecurityConfig {
                 return password;
             }
             return NOOP_PASSWORD_PREFIX + password;
+        }
+    }
+
+    /**
+     * Support use OAuth Provider's UserNameAttribute for JwtAuthentication name
+     * Multiple providers use jwt claim 'origin' to determine uniqueness
+     */
+    public static class OAuth2ClientProviderUserNameAttributeJwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+        private OAuth2ClientProperties oAuth2ClientProperties;
+        private Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter
+                = new JwtGrantedAuthoritiesConverter();
+
+        public OAuth2ClientProviderUserNameAttributeJwtAuthenticationConverter(OAuth2ClientProperties oAuth2ClientProperties) {
+            this.oAuth2ClientProperties = oAuth2ClientProperties;
+        }
+
+        private String getUserNameAttribute(Jwt jwt){
+            return Optional.ofNullable(jwt.getClaimAsString("origin"))
+                    .map(origin -> oAuth2ClientProperties.getProvider().get(origin))
+                    .map(OAuth2ClientProperties.Provider::getUserNameAttribute)
+                    .map(jwt::getClaimAsString)
+                    .orElse(jwt.getSubject());
+        }
+
+        @Override
+        public AbstractAuthenticationToken convert(Jwt jwt) {
+            Collection<GrantedAuthority> authorities = extractAuthorities(jwt);
+            return new JwtAuthenticationToken(jwt, authorities, getUserNameAttribute(jwt));
+        }
+
+        @Deprecated
+        protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
+            return this.jwtGrantedAuthoritiesConverter.convert(jwt);
+        }
+
+        public void setJwtGrantedAuthoritiesConverter(Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter) {
+            Assert.notNull(jwtGrantedAuthoritiesConverter, "jwtGrantedAuthoritiesConverter cannot be null");
+            this.jwtGrantedAuthoritiesConverter = jwtGrantedAuthoritiesConverter;
         }
     }
 
